1a9b500fddc1fe8c681f1e576aa7b76db97ef255
Tyler Maginnis <tylermaginnis@users.noreply.github.com>
2024-08-21 04:05:43 -0400

    Add the Multi-Site Eventing Base Calendar View

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -1,11 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using MultisiteEventing.Models;
+using System.Collections.Generic;
 
 namespace MultisiteEventing.Controllers
 {
     public class CalendarController : Controller
     {
+        private readonly IMemoryCache _cache;
+        private const string ClientsCacheKey = "ClientsCacheKey";
+        private const string EventsCacheKey = "EventsCacheKey";
+
+        public CalendarController(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+            if (!_cache.TryGetValue(ClientsCacheKey, out List<Client> _))
+            {
+                _cache.Set(ClientsCacheKey, new List<Client>());
+            }
+            if (!_cache.TryGetValue(EventsCacheKey, out List<Event> _))
+            {
+                _cache.Set(EventsCacheKey, new List<Event>());
+            }
+        }
+
         public IActionResult Index()
         {
+            ViewData["Clients"] = _cache.Get<List<Client>>(ClientsCacheKey);
+            ViewData["Events"] = _cache.Get<List<Event>>(EventsCacheKey);
             return View();
         }
     }
